7e997d42989506eab0f54c4b958f58a4a4903727
Lince99 <lynxnic99@gmail.com>
2019-05-02 17:02:39 +0200

    Fix typoes on make.sh

--- make.sh
+++ make.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#TODO FIX PACKAGE TREE
 mkdir -p Release
 echo "Compiling Core..."
 javac -d Release Tabagism_core/*.java
@@ -7,7 +8,8 @@ javac -classpath /usr/local/share/java/gtk-4.1.jar -d Release Tabagism_GUI/GUI_M
 echo "Creating jar..."
 jar -cvmf MANIFEST.MF Release/JTabagism.jar Release/Tabagism/Tabagism_core/Main.class
 echo "Removing Compilated classes..."
-rm -rf Release/JTabagism
+rm -rf Release/Tabagism/
 echo "Executing JTabagism..."
-#TODO FIX THIS
 java -jar Release/JTabagism.jar
+echo "Project Directory tree:"
+#tree
